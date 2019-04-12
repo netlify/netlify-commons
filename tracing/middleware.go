14fe86cecd1a560f529cf3af8e9502ca79531e86
Ryan <ry.neal@gmail.com>
2019-04-11 17:22:07 -0700

    make middleware actually middleware

--- tracing/middleware.go
+++ tracing/middleware.go
@@ -6,8 +6,10 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func NewTracingMiddleware(log logrus.FieldLogger, service string, next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		TrackRequest(w, r, log, service, next)
-	})
+func Middleware(log logrus.FieldLogger, svcName string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			TrackRequest(w, r, log, svcName, next)
+		})
+	}
 }
