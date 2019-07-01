992add9ab0725f80b17aab951de17f9faa87a4a3
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2019-07-01 13:12:45 -0700

    Use ConnectStart instead of DNS lookup hook for safe http

--- http/http_test.go
+++ http/http_test.go
@@ -10,15 +10,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIsLocalAddress(t *testing.T) {
-	assert.False(t, isLocalAddress("216.58.194.206"))
-	assert.True(t, isLocalAddress("127.0.0.1"))
-	assert.True(t, isLocalAddress("10.0.0.1"))
-	assert.True(t, isLocalAddress("192.168.0.1"))
-	assert.True(t, isLocalAddress("172.16.0.0"))
-	assert.True(t, isLocalAddress("169.254.169.254"))
-}
-
 type countServer struct {
 	count int
 }
@@ -48,6 +39,13 @@ func TestSafeHTTPClient(t *testing.T) {
 	client = SafeHTTPClient(client, logrus.New())
 
 	res, err = client.Get("http://localhost:9999")
+	assert.NotNil(t, err)
+	assert.Nil(t, res)
+	assert.Equal(t, 1, counter.count)
+
+	fmt.Println("Trying ip based req")
+	res, err = client.Get("http://169.254.169.254:9999")
+	fmt.Printf("Got res %v and err %v", res, err)
 	assert.NotNil(t, err)
 	assert.Nil(t, res)
 	assert.Equal(t, 1, counter.count)
