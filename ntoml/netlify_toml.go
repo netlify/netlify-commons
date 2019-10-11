d3eef86e4431d17a39aeb4dfabd659875eac96d4
Shayon Mukherjee <shayon@netlify.com>
2019-10-11 07:50:12 -0700

    Fix rebase mess

--- ntoml/netlify_toml.go
+++ ntoml/netlify_toml.go
@@ -36,11 +36,11 @@ type Settings struct {
 }
 
 type BuildConfig struct {
-	Command     string            `toml:"command"`
-	Base        string            `toml:"base"`
-	Publish     string            `toml:"publish"`
-	Ignore      string            `toml:"ignore"`
-	Environment map[string]string `toml:"environment"`
+	Command     string            `toml:"command" json:"command" yaml:"command"`
+	Base        string            `toml:"base" json:"base" yaml:"base"`
+	Publish     string            `toml:"publish" json:"publish" yaml:"publish"`
+	Ignore      string            `toml:"ignore" json:"ignore" yaml:"ignore"`
+	Environment map[string]string `toml:"environment" json:"environment" yaml:"environment"`
 }
 
 type DeployContext struct {
