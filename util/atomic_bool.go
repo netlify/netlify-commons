c3114d418e58cd2e0750167803cdda694e277c22
Mike Heffner <mikeh@fesnel.com>
2020-04-01 17:20:01 -0400

    Pull in AtomicBool from eventeer.

---
+++ util/atomic_bool.go
@@ -0,0 +1,39 @@
+package util
+
+import "sync/atomic"
+
+const (
+	falseValue int32 = 0
+	trueValue  int32 = 1
+)
+
+type AtomicBool struct {
+	value int32
+}
+
+func NewAtomicBool(val bool) *AtomicBool {
+	a := &AtomicBool{value: falseValue}
+	a.Set(val)
+	return a
+}
+
+// Set will set the value to boolValue and will return the previous value
+func (a *AtomicBool) Set(boolValue bool) bool {
+	intValue := int32(falseValue)
+	if boolValue {
+		intValue = trueValue
+	}
+	return a.toTruthy(atomic.SwapInt32(&a.value, intValue))
+}
+
+// Get will return the current value
+func (a *AtomicBool) Get() bool {
+	return a.toTruthy(atomic.LoadInt32(&a.value))
+}
+
+func (a *AtomicBool) toTruthy(val int32) bool {
+	if val == falseValue {
+		return false
+	}
+	return true
+}
