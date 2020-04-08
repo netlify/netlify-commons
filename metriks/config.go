ed7b3469b7d614c8733ac5c983fb2d9395ed9626
Mike Heffner <mikeh@fesnel.com>
2020-04-07 21:49:21 -0400

    Comment typo.

--- metriks/config.go
+++ metriks/config.go
@@ -4,7 +4,7 @@ type Config struct {
 	Host string `default:"localhost"`
 	Port int    `default:"8125"`
 
-	// Name is the typically the local hostname or pod name
+	// Name is typically the local hostname or pod name
 	Name string `default:"local"`
 	Tags map[string]string
 }
