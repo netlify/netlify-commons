50c869344e272eb9dafb73cb3c5eb66b66d643f1
Ryan <ry.neal@gmail.com>
2020-08-06 11:20:51 -0700

    make tracing and metrics part of the default config

--- metriks/config.go
+++ metriks/config.go
@@ -1,8 +1,9 @@
 package metriks
 
 type Config struct {
-	Host string `default:"localhost"`
-	Port int    `default:"8125"`
+	Enabled bool
+	Host    string `default:"localhost"`
+	Port    int    `default:"8125"`
 
 	Tags map[string]string
 }
