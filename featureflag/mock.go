1efc6d99964f4aa1f3a94688a30533c5ee49bf8a
Mike Heffner <mikeh@fesnel.com>
2020-09-23 11:40:58 -0400

    Expose LD users to enable custom attributes.

--- featureflag/mock.go
+++ featureflag/mock.go
@@ -1,5 +1,7 @@
 package featureflag
 
+import ld "gopkg.in/launchdarkly/go-server-sdk.v4"
+
 type MockClient struct {
 	BoolVars   map[string]bool
 	StringVars map[string]string
@@ -7,11 +9,19 @@ type MockClient struct {
 
 var _ Client = MockClient{}
 
-func (c MockClient) Enabled(key, _ string) bool {
+func (c MockClient) Enabled(key, userID string) bool {
+	return c.EnabledUser(key, ld.NewUser(userID))
+}
+
+func (c MockClient) EnabledUser(key string, _ ld.User) bool {
 	return c.BoolVars[key]
 }
 
-func (c MockClient) Variation(key, defaultVal, _ string) string {
+func (c MockClient) Variation(key string, defaultVal string, userID string) string {
+	return c.VariationUser(key, defaultVal, ld.NewUser(userID))
+}
+
+func (c MockClient) VariationUser(key string, defaultVal string, _ ld.User) string {
 	res, ok := c.StringVars[key]
 	if !ok {
 		return defaultVal
