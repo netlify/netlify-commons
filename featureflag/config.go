c40f200c55dc239db0bffd9c3b7d0699b810f9ba
Marcus Weiner <marcus.weiner@gmail.com>
2020-12-15 14:47:20 +0100

    Add yaml tags

--- featureflag/config.go
+++ featureflag/config.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Config struct {
-	Key            string        `json:"key"`
-	RequestTimeout time.Duration `json:"request_timeout" mapstructure:"request_timeout" split_words:"true" default:"5s"`
-	Enabled        bool          `json:"enabled" default:"false"`
+	Key            string        `json:"key" yaml:"key"`
+	RequestTimeout time.Duration `json:"request_timeout" yaml:"request_timeout" mapstructure:"request_timeout" split_words:"true" default:"5s"`
+	Enabled        bool          `json:"enabled" yaml:"enabled" default:"false"`
 
 	updateProcessorFactory ld.UpdateProcessorFactory `json:"-"`
 
 	// Drop telemetry events (not needed in local-dev/CI environments)
-	DisableEvents bool `json:"disable_events" mapstructure:"disable_events" split_words:"true"`
+	DisableEvents bool `json:"disable_events" yaml:"disable_events" mapstructure:"disable_events" split_words:"true"`
 
 	// Set when using the Launch Darkly Relay proxy
-	RelayHost string `json:"relay_host" mapstructure:"relay_host" split_words:"true"`
+	RelayHost string `json:"relay_host" yaml:"relay_host" mapstructure:"relay_host" split_words:"true"`
 }
