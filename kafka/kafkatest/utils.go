cdca0bfb6930ba5e46e33b40c20eeb9e3f2c7aef
Kelsey <kelsey@netlify.com>
2021-03-12 13:59:19 -0700

    Change uncommitedOffset to readOffset

--- kafka/kafkatest/utils.go
+++ kafka/kafkatest/utils.go
@@ -33,13 +33,13 @@ func KafkaPipe(log logrus.FieldLogger) (*FakeKafkaConsumer, *FakeKafkaProducer) 
 }
 
 type FakeKafkaConsumer struct {
-	messages         []*kafkalib.Message
-	msgMu            sync.Mutex
-	offset           int64
-	uncommitedOffset int64
-	notify           chan struct{}
-	commits          chan *kafkalib.Message
-	log              logrus.FieldLogger
+	messages   []*kafkalib.Message
+	msgMu      sync.Mutex
+	offset     int64
+	readOffset int64
+	notify     chan struct{}
+	commits    chan *kafkalib.Message
+	log        logrus.FieldLogger
 }
 
 type FakeKafkaProducer struct {
@@ -64,12 +64,12 @@ func (f *FakeKafkaProducer) Close() error {
 
 func NewFakeKafkaConsumer(log logrus.FieldLogger, distri <-chan *kafkalib.Message) *FakeKafkaConsumer {
 	r := &FakeKafkaConsumer{
-		messages:         make([]*kafkalib.Message, 0),
-		offset:           0,
-		uncommitedOffset: 0,
-		notify:           make(chan struct{}),
-		log:              log,
-		commits:          make(chan *kafkalib.Message, 1000),
+		messages:   make([]*kafkalib.Message, 0),
+		offset:     0,
+		readOffset: 0,
+		notify:     make(chan struct{}),
+		log:        log,
+		commits:    make(chan *kafkalib.Message, 1000),
 	}
 
 	go func() {
@@ -88,12 +88,12 @@ func NewFakeKafkaConsumer(log logrus.FieldLogger, distri <-chan *kafkalib.Messag
 func (f *FakeKafkaConsumer) FetchMessage(ctx context.Context) (*kafkalib.Message, error) {
 	for {
 		f.msgMu.Lock()
-		if int64(len(f.messages)) > f.uncommitedOffset {
-			f.log.WithField("offset", f.uncommitedOffset).Trace("offering message")
-			msg := f.messages[f.uncommitedOffset]
+		if int64(len(f.messages)) > f.readOffset {
+			f.log.WithField("offset", f.readOffset).Trace("offering message")
+			msg := f.messages[f.readOffset]
 			f.msgMu.Unlock()
 
-			f.uncommitedOffset = f.uncommitedOffset + 1
+			f.readOffset = f.readOffset + 1
 			return msg, nil
 		}
 		f.msgMu.Unlock()
@@ -111,7 +111,6 @@ func (f *FakeKafkaConsumer) CommitMessage(msg *kafkalib.Message) error {
 	f.log.WithField("offset", msg.TopicPartition.Offset).Trace("commiting message...")
 	if int64(msg.TopicPartition.Offset) > f.offset {
 		f.offset = int64(msg.TopicPartition.Offset)
-		f.uncommitedOffset = f.offset
 		f.log.WithField("offset", f.offset).Trace("set new offset")
 	}
 	select {
@@ -125,15 +124,14 @@ func (f *FakeKafkaConsumer) CommitMessage(msg *kafkalib.Message) error {
 func (f *FakeKafkaConsumer) SetInitialOffset(offset int64) error {
 	f.msgMu.Lock()
 	f.offset = offset
-	f.uncommitedOffset = offset
+	f.readOffset = offset
 	f.msgMu.Unlock()
 	return nil
 }
 
 func (f *FakeKafkaConsumer) Seek(offset int64) error {
 	f.msgMu.Lock()
-	f.offset = offset
-	f.uncommitedOffset = offset
+	f.readOffset = offset
 	f.msgMu.Unlock()
 	return nil
 }
