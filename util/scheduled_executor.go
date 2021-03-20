e18e3f94bfa927af664b88f78f73aed114bad28b
Emily Zhang <emilyzhang@netlify.com>
2021-03-19 17:54:11 -0700

    fix re: mike & cole's comments

--- util/scheduled_executor.go
+++ util/scheduled_executor.go
@@ -10,32 +10,66 @@ type ScheduledExecutor interface {
 	Stop()
 }
 
+type CustomTicker interface {
+	Stop()
+	C() <-chan time.Time
+}
+
+type Option func(*scheduledExecutor)
+
+type defaultTicker struct {
+	*time.Ticker
+}
+
+func (d *defaultTicker) C() <-chan time.Time {
+	return d.Ticker.C
+}
+
 type scheduledExecutor struct {
-	period             time.Duration
-	cb                 func()
-	isRunning          AtomicBool
-	ticker             *time.Ticker
-	done               chan bool
-	wg                 sync.WaitGroup
-	enableInstantStart bool
+	period       time.Duration
+	cb           func()
+	isRunning    AtomicBool
+	ticker       CustomTicker
+	done         chan bool
+	wg           sync.WaitGroup
+	initialDelay time.Duration
 }
 
 func NewScheduledExecutor(period time.Duration, cb func()) ScheduledExecutor {
 	return &scheduledExecutor{
-		period:    period,
-		cb:        cb,
-		isRunning: NewAtomicBool(false),
-		wg:        sync.WaitGroup{},
+		period:       period,
+		cb:           cb,
+		isRunning:    NewAtomicBool(false),
+		wg:           sync.WaitGroup{},
+		initialDelay: period,
 	}
 }
 
-func NewExecutorWithInstantStart(period time.Duration, cb func()) ScheduledExecutor {
-	return &scheduledExecutor{
-		period:             period,
-		cb:                 cb,
-		isRunning:          NewAtomicBool(false),
-		wg:                 sync.WaitGroup{},
-		enableInstantStart: true,
+func NewScheduledExecutorWithOpts(period time.Duration, cb func(), options ...Option) ScheduledExecutor {
+	s := &scheduledExecutor{
+		period:       period,
+		cb:           cb,
+		isRunning:    NewAtomicBool(false),
+		wg:           sync.WaitGroup{},
+		initialDelay: period,
+	}
+
+	for _, opt := range options {
+		opt(s)
+	}
+
+	return s
+}
+
+func WithInitialDelay(initialDelay time.Duration) Option {
+	return func(s *scheduledExecutor) {
+		s.initialDelay = initialDelay
+	}
+}
+
+func WithCustomTicker(ticker CustomTicker) Option {
+	return func(s *scheduledExecutor) {
+		s.ticker = ticker
 	}
 }
 
@@ -44,7 +78,7 @@ func (s *scheduledExecutor) Start() {
 		return
 	}
 
-	s.ticker = time.NewTicker(s.period)
+	s.ticker = CustomTicker(&defaultTicker{time.NewTicker(s.period)})
 	s.done = make(chan bool)
 	s.wg.Add(1)
 
@@ -66,23 +100,13 @@ func (s *scheduledExecutor) Stop() {
 
 func (s *scheduledExecutor) poll() {
 	defer s.wg.Done()
-	if s.enableInstantStart {
-		for ; true; <-s.ticker.C {
-			select {
-			case <-s.done:
-				return
-			default:
-			}
-			s.cb()
+	time.Sleep(s.initialDelay)
+	for ; true; <-s.ticker.C() {
+		select {
+		case <-s.done:
+			return
+		default:
 		}
-	} else {
-		for {
-			select {
-			case <-s.done:
-				return
-			case <-s.ticker.C:
-				s.cb()
-			}
-		}
+		s.cb()
 	}
 }
