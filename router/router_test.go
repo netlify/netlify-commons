c16d77d446be5bd01fde0af2650d8701e0d068e2
Ryan Neal <rybit@users.noreply.github.com>
2022-01-24 10:25:15 -0800

    chore: update how we get request IDs (#313)
    * update how we get request IDs
    
    * test that we log properly
    
    * also test that the tracer is injected

--- router/router_test.go
+++ router/router_test.go
@@ -6,6 +6,8 @@ import (
 	"strconv"
 	"testing"
 
+	"github.com/netlify/netlify-commons/testutil"
+	"github.com/netlify/netlify-commons/tracing"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/mocktracer"
 	"github.com/sirupsen/logrus"
@@ -79,11 +81,13 @@ func TestTracing(t *testing.T) {
 	}()
 
 	noop := func(w http.ResponseWriter, r *http.Request) error {
+		assert.NotNil(t, tracing.GetTracer(r))
 		w.WriteHeader(http.StatusOK)
 		return nil
 	}
 
-	r := New(logrus.WithField("test", t.Name()), OptEnableTracing("some-service"))
+	tl, logHook := testutil.TestLogger(t)
+	r := New(tl, OptEnableTracing("some-service"))
 
 	r.Method(http.MethodPatch, "/patch", noop)
 	r.Delete("/abc/{def}", noop)
@@ -110,6 +114,7 @@ func TestTracing(t *testing.T) {
 	for name, scene := range scenes {
 		t.Run(name, func(t *testing.T) {
 			mt.Reset()
+			logHook.Reset()
 
 			rec := httptest.NewRecorder()
 			r.ServeHTTP(rec, httptest.NewRequest(scene.method, scene.path, nil))
@@ -121,6 +126,8 @@ func TestTracing(t *testing.T) {
 				assert.Equal(t, scene.resourceName, spans[0].Tag(ext.ResourceName))
 				assert.Equal(t, strconv.Itoa(http.StatusOK), spans[0].Tag(ext.HTTPCode))
 			}
+			// should be a starting and finished request for each request
+			assert.Len(t, logHook.AllEntries(), 2)
 		})
 	}
 }
